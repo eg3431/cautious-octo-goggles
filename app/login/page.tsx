93dcc36cab85c0c6acb2f1fe29c204acdf400ab4
Erica Guo <eg3431@columbia.edu>
2026-02-10 16:56:33 -0500

    Added console log key statements

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -3,6 +3,9 @@
 import { createBrowserClient } from '@supabase/ssr'
 
 export default function Login() {
+    console.log("printing keys")
+    console.log(process.env.NEXT_PUBLIC_SUPABASE_URL)
+    console.log(process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY)
   const supabase = createBrowserClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
